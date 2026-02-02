aad35ffe51f76b17bcee601e27e63e2caf39fd9b
Wagner Quirino <wagner.quirino@calindra.com.br>
2026-02-02 19:10:01 -0300

    Bumped versions across templates for configuration alignment and dependency updates.

--- shopping-shopify-template-cart/eitri-app.conf.js
+++ shopping-shopify-template-cart/eitri-app.conf.js
@@ -4,7 +4,7 @@ module.exports = {
 	'slug': 'shopping-shopify-template-cart',
 	'eitri-luminus': '2.22.3',
 	'eitri-bifrost': '4.7.0',
-	'version': '0.1.5',
+	'version': '0.1.6',
 	'public-key': '9510e0dd-de42-4842-8ba8-78ef1f4a70d2',
 	'applicationId': 'cca11cae-342e-4ca6-a955-00abca321d86',
 	'id': '9fa4d1d7-6a48-4a58-83bc-853ed74020e1',
@@ -19,7 +19,7 @@ module.exports = {
 		},
 		'shopping-shopify-template-shared': {
 			isEitriAppShared: true,
-			version: '0.1.2'
+			version: '0.1.3'
 		},
 		'i18n': {
 			version: '14.1.2'
